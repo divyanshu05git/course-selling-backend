48affed78e32257bc5bfe46205bea1e83fff9259
divyanshu05git <divyanshupratapsingh05@gmail.com>
2025-06-24 02:39:13 +0530

    course routes has been working

--- middlewares/userMiddleware.js
+++ middlewares/userMiddleware.js
@@ -3,16 +3,22 @@ const {JWT_SECRET} =require("../config")
 
 function userAuth(req,res,next){
     const token=req.headers.token;
-    const decoded=jwt.verify(token,JWT_SECRET);
 
-    if(decode){
-        req.userId=decoded.id;
-        next()
-    }
-    else{
-        res.status(403).json({
-            message:"you are not signed in"
+    if(!token){
+        res.status(401).json({
+            message:"token missing"
         })
+    }
+
+    try{
+        const decoded=jwt.verify(token,JWT_SECRET);
+        req.userId = decoded.id;
+        next();
+    }
+    catch(err){
+        return res.status(403).json({
+            message: "Invalid or expired token"
+        });
     }
 }
 
--- routes/course.js
+++ routes/course.js
@@ -1,15 +1,31 @@
 const {Router}=require("express")
 const jwt=require("jsonwebtoken")
 const {JWT_SECRET} =require("../config")
+const {userAuth}=require("../middlewares/userMiddleware")
+const {purchaseModel,courseModel}=require("../db")
 
 const courseRouter=Router();
 
-courseRouter.post("/purchase",(req,res)=>{
+courseRouter.post("/purchase",userAuth,async(req,res)=>{
+    const userId=req.userId;
+    const courseId=req.body.courseId;
 
+    await purchaseModel.create({
+        userId,
+        courseId
+    })
+
+    res.json({
+        message:"course hase been bought"
+    })
 })
 
-courseRouter.get("/preview",(req,res)=>{
+courseRouter.get("/preview",userAuth,async(req,res)=>{
+    const courses=await courseModel.find({})
 
+    res.json({
+        courses
+    })
 })
 
 module.exports={
